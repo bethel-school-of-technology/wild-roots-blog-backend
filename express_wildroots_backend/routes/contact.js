4380d4ac125714571d7890844a1092902e92cfbb
Sonya <sonyalaird31@gmail.com>
2020-12-18 17:30:42 -0700

    Updated Contact.js

---
+++ express_wildroots_backend/routes/contact.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+var cors = require('cors');
+const creds = require('./config');
+
+import transporter from transporter;
+
+var transport = {
+  host: 'smtp.example.com',
+  port: 1025,
+  auth: {
+    user: creds.USER,
+    pass: creds.PASS
+  }
+},
+var transporter = nodemailer.createTransport(transport)
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Server is ready to take messages');
+  }
+});
+
+router.post('/send', (req, res, next) => {
+  var name = req.body.name
+  var email = req.body.email
+  var message = req.body.message
+  var content = `name: ${name} \n email: ${email} \n message: ${message} `
+  var mail = {
+    from: name,
+    to:'RECEIVING_EMAIL_ADDRESS_GOES_HERE',  // Change email address 
+    subject: 'New Message from Contact Form',
+    text: content
+  }
+  
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: 'fail'
+      })
+    } else {
+   
+        transporter.sendMail({
+            from: "<your email address>",
+            to: email,
+            subject: "Submission was successful",
+            text: `Thank you for contacting us!\n\nForm details\nName: ${name}\nEmail: ${email}\nMessage: ${message}`
+        }, function(error, info){
+            if(error) {
+                console.log(error);
+            } else{
+                console.log('Message sent: ' + info.response);
+            }
+        })
+
+
+        res.json({
+         status: 'success'
+        })
+    }
+  })
+})
+
+
+
+  
+  module.exports = router;
