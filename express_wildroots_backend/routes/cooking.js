a8b3487d01feed70c4c87a899d7624b60208af12
Anthony <anthonydesignwarrior@protonmail.com>
2020-12-24 05:00:22 -0800

    added cooking routes file and gardening file and corresponding models and debugged dependencies so back end can render

--- express_wildroots_backend/routes/cooking.js
+++ express_wildroots_backend/routes/cooking.js
@@ -32,3 +32,56 @@ router.get('/:id', async function(req, res) {
         });
     }
 });
+
+// you have to use async in order to use await
+router.post('/add', async function(req, res){
+    try {
+        const newRecipie = await Cooking.create(req.body); 
+
+            res.status(200).json({
+                data: { Recipie: newRecipie }
+            });
+        } catch (err) {
+            res.status(404).json({
+                status: 'fail',
+                message: err
+            });
+        }
+});
+
+
+router.put('/update/:id', async function(req, res){
+    try {
+        const RecipieUpdate = await Cooking.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+            res.status(200).json({
+                data: { RecipieUpdate: RecipieUpdate }
+            });
+        } catch (err) {
+            res.status(500).json({
+                status: 'fail',
+                message: err
+            });
+        }
+});
+
+
+router.delete('/delete/:id', async function(req, res){
+    try {
+        await Cooking.findByIdAndDelete(req.params.id); 
+        res.status(200).json({
+            status: 'success',
+            data: null
+            });
+        } catch (err) {
+            res.status(500).json({
+                status: 'fail',
+                message: err
+            });
+        }
+});
+
+module.exports = router;
